d040adf2ea0f45529bf0c61176d7b8903f026d91
purushuttam <55408162+purushuttam@users.noreply.github.com>
2023-06-15 00:47:15 +0530

    fixed

--- app.js
+++ app.js
@@ -25,11 +25,7 @@ app.use(express.json())
 app.use(cookieParser());
 
 app.use(
-  cors({
-    origin: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+  cors()
 );
 
 // app.get('*', checkUser); // apply in every routes
