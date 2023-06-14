b31de9c9d403b973a42839d1a9a7c65b40ec30c1
purushuttam <55408162+purushuttam@users.noreply.github.com>
2023-06-15 01:28:19 +0530

    url changed

--- routes/network/network.router.js
+++ routes/network/network.router.js
@@ -2,10 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/GetLocalIpAddress', (req, res) => {
+router.get('/GetIPDetails', (req, res) => {
     const clientIpAddress = req.ip;
     const remoteIpAddress = req.connection.remoteAddress;
-    const serverIpAddress = req.headers['x-forwarded-for'] || remoteIpAddress;
+    const serverIpAddress = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || remoteIpAddress;
     //const remoteIpAddress = req.connection.remoteAddress;
     console.log('clientIpAddress : ' + clientIpAddress);
     console.log('remote ip address : ' + remoteIpAddress);
