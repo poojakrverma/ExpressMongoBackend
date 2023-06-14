af9576bf9afd9f6fdddb6b29393b3f7c37b00996
purushuttam <55408162+purushuttam@users.noreply.github.com>
2023-06-15 01:32:03 +0530

    sockeet

--- routes/network/network.router.js
+++ routes/network/network.router.js
@@ -6,6 +6,7 @@ router.get('/GetIPDetails', (req, res) => {
     const clientIpAddress = req.ip;
     const remoteIpAddress = req.connection.remoteAddress;
     const serverIpAddress = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || remoteIpAddress;
+    const socketIpAddress = req.socket.localAddress;
     //const remoteIpAddress = req.connection.remoteAddress;
     console.log('clientIpAddress : ' + clientIpAddress);
     console.log('remote ip address : ' + remoteIpAddress);
@@ -13,7 +14,8 @@ router.get('/GetIPDetails', (req, res) => {
     res.send({
         "clientIpAddress": clientIpAddress,
         "remoteIpAddress": remoteIpAddress,
-        "serverIpAddress": serverIpAddress
+        "serverIpAddress": serverIpAddress,
+        "socketIpAddress": socketIpAddress
     })
 })
 
