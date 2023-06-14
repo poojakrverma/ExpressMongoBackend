f1d72d9546908ce3005a34f7c545e9bcdfb02a5a
purushuttam <55408162+purushuttam@users.noreply.github.com>
2023-06-15 00:11:55 +0530

    cnn

--- routes/network/network.router.js
+++ routes/network/network.router.js
@@ -5,12 +5,15 @@ const router = express.Router();
 router.get('/GetLocalIpAddress', (req, res) => {
     const remoteIpAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const localIpAdress = req.ip;
+    const localAddress = req.connection.localAddress;
+    const address = req.connection.address();
     //const remoteIpAddress = req.connection.remoteAddress;
     console.log('local ip address : ' + localIpAdress);
     console.log('remote ip address : ' + remoteIpAddress)
     res.send({
         "local Ip Address": localIpAdress,
-        "remote Ip Address": remoteIpAddress
+        "remote Ip Address": remoteIpAddress,
+        "address": address,
     })
 })
 
